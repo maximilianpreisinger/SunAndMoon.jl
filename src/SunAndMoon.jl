05ee72cdc2885e707fc9b17522e7bc3a76f1488d
Maximilian Preisinger <maximilian.preisinger1@gmail.com>
2018-09-18 11:08:49 +0200

    Adding about() function

---
+++ src/SunAndMoon.jl
@@ -0,0 +1,17 @@
+module SunAndMoon
+
+about() = print(raw"
+       --- Sun and Moon ---
+         .               _.._
+       \ | /           .' .-'`
+     '-.;;;.-'        /  /
+    -==;;;;;==-       |  |
+     .-';;;'-.        \  \
+       / | \           '._'-._
+ jgs     '                ```
+(Ascii Art from: http://www.oocities.org/spunk1111/celestal.htm)
+
+This package shall provide some utilities for calculating the time for sunrise and moonrise for any position on earth.
+")
+
+end # module
